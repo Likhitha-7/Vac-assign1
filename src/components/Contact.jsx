fbbb6271f5fcb108b8c94f98d2dcd7fd47c6caf5
Likhitha-7 <likhithareddy7000@outlook.com>
2021-07-16 18:09:14 +0530

    assignment

---
+++ src/components/Contact.jsx
@@ -0,0 +1,33 @@
+import React from "react";
+
+function Contact() {
+  return (
+    <div className="contact">
+      <div class="container">
+        <div class="row align-items-center my-5">
+          <div class="col-lg-7">
+            <img
+              class="img-fluid rounded mb-4 mb-lg-0"
+              src="https://presidencyuniversity.in/content/uploads/2017/07/presidency-university-campus-Infra.jpg"
+              alt=""
+            />
+          </div>
+          <div class="col-lg-5">
+            <h1 class="font-weight-light">Contact</h1>
+            <p>
+                Name: Likhitha Mallu
+            </p>
+            <p>
+                ID:20181CSE0396
+            </p>
+            <p>
+                Subject: VAC FED
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Contact;
---
+++ src/components/Home.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+
+function Home() {
+  return (
+    <div className="home">
+      <div class="container">
+        <div class="row align-items-center my-5">
+          <div class="col-lg-7">
+            <img
+              class="img-fluid rounded mb-4 mb-lg-0"
+              src="https://presidencyuniversity.in/content/uploads/2017/07/presidency-university-campus-Infra.jpg"
+              alt=""
+            />
+          </div>
+          <div class="col-lg-5">
+            <h1 class="font-weight-light">Home</h1>
+            <h3 class="font-weight-light">Hello All!</h3>
+            
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Home;
